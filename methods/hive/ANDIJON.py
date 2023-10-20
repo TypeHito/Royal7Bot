3bfa4de771aeff3ba6879a03fa6de68ff2c02713
TypeHito <NoneType.Cardinal@gmail.com>
2023-10-20 15:40:07 +0500

    Update ANDIJON.py

--- methods/hive/ANDIJON.py
+++ methods/hive/ANDIJON.py
@@ -5,7 +5,7 @@ from urllib3.exceptions import InsecureRequestWarning
 import requests
 
 USERNAME = 'hoji2'
-PASSWORD = 'hojiakbar2101200518'
+PASSWORD = 'ddsak34964364'
 API_HOST = "https://royaltaxi-andijon.hivelogin.ru"
 
 HEADERS = {
